32a0143d132c0a202995614eb7b3c4689e67f607
saeko1996 <48708282+saeko1996@users.noreply.github.com>
2020-01-24 10:42:20 +0800

    Add files via upload

---
+++ fileupload.php
@@ -0,0 +1,14 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<title>File Uploading</title>
+</head>
+<body>
+    <form action="uploadtoserver.php" method="post" enctype="multipart/form-data">
+<label>Upload An Image</label>
+<input type="file" name="file_upload" />
+<input type="submit" name="upload"/>
+</form>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>renzShoe</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/pricing/">
+    <link rel="stylesheet" href="../online_selling/bootstrap4/css/bootstrap.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="pricing.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal">LEBRON NIKE SHOES SALE!</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Features</a>
+        <a class="p-2 text-dark" href="#">Enterprise</a>
+        <a class="p-2 text-dark" href="#">Support</a>
+        <a class="p-2 text-dark" href="#">Pricing</a>
+      </nav>
+      <a style="margin: 5px;" class="btn btn-outline-primary" href="login.php">Login</a>
+      <a class="btn btn-outline-primary" href="signup.php">Sign up</a>
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Lebron Nike Shoes</h1>
+      <p class="lead">Attack the rack and dismantle your opponents with supreme strength and confidence in men's LeBron shoes.</p>
+    </div>
+
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal"></h4>
+          </div>
+          <div class="card-body">
+          
+            <h1 class="card-title pricing-card-title"><small class="text-muted"></small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+       <!-- <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">
+          </div>
+          <div class="card-body">
+          
+            <h1 class="card-title pricing-card-title"><small class="text-muted"></small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+             
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary"></button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal"></h4>
+          </div>
+          <div class="card-body">
+          
+            <h1 class="card-title pricing-card-title"> <small class="text-muted"></small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary"></button>
+          </div>
+        </div>
+      </div>
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted"></small>
+          </div>
+          <div class="col-6 col-md">
+            <h5></h5>
+            <ul class="list-unstyled text-small">
+              
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5></h5>
+            <ul class="list-unstyled text-small">
+              
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5></h5>
+            <ul class="list-unstyled text-small">
+              
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+  </body>
+</html>
